bad10d1b0ba945f638537a67d9ec95b19ed387dc
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2023-06-23 16:20:51 -0300

    Navmenu is working (I guess)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [mdx()]
+  integrations: [mdx(), react()]
 });
