1813f9cf911b17d024380be0b6c1a0f6563d30c1
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2023-06-27 21:16:50 -0300

    Remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [mdx(), react()]
+  integrations: [mdx()]
 });
