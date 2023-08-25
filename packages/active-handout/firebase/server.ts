0beafaa56f2065ef413972f06208f55dcfcb07ec
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2023-08-25 16:10:13 -0500

    Add verification for env vars

--- packages/active-handout/firebase/server.ts
+++ packages/active-handout/firebase/server.ts
@@ -16,6 +16,31 @@ const serviceAccount = {
   client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
 };
 
+if (config.auth) {
+  const missingVars = [];
+  if (!serviceAccount.project_id)
+    missingVars.push("PUBLIC_FIREBASE_PROJECT_ID");
+  if (!serviceAccount.private_key_id)
+    missingVars.push("FIREBASE_PRIVATE_KEY_ID");
+  if (!serviceAccount.private_key) missingVars.push("FIREBASE_PRIVATE_KEY");
+  if (!serviceAccount.client_email) missingVars.push("FIREBASE_CLIENT_EMAIL");
+  if (!serviceAccount.client_id) missingVars.push("FIREBASE_CLIENT_ID");
+  if (!serviceAccount.auth_uri) missingVars.push("FIREBASE_AUTH_URI");
+  if (!serviceAccount.token_uri) missingVars.push("FIREBASE_TOKEN_URI");
+  if (!serviceAccount.auth_provider_x509_cert_url)
+    missingVars.push("FIREBASE_AUTH_CERT_URL");
+  if (!serviceAccount.client_x509_cert_url)
+    missingVars.push("FIREBASE_CLIENT_CERT_URL");
+
+  if (missingVars.length > 0) {
+    throw new Error(
+      `Missing environment variables for firebase auth: ${missingVars.join(
+        ", "
+      )}`
+    );
+  }
+}
+
 let app: App | undefined;
 try {
   if (config.auth) {
